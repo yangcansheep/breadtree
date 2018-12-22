9f3086db9a5516f7e4ae7ce97cf01688aa8dbe85
yangcan <can.yang@rokid.com>
2018-12-22 17:44:56 +0800

    first commit

---
+++ deer-start/src/main/java/deer/DeerApplication.java
@@ -0,0 +1,44 @@
+package deer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.MultipartConfigElement;
+
+@Slf4j
+@RestController
+@SpringBootApplication
+@MapperScan("deer.dao.mapper")
+public class DeerApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(DeerApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	String home() {
+		return "This is JupiterApplication.";
+	}
+	/**
+	 * 文件上传配置
+	 *
+	 * @return
+	 */
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		//文件最大
+		factory.setMaxFileSize("1024MB"); //KB,MB
+		/// 设置总上传数据总大小
+		factory.setMaxRequestSize("1024MB");
+		return factory.createMultipartConfig();
+	}
+
+}
